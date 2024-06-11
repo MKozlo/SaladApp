f27496744cbe958cbad63299f56b854b893a92eb
shiro <shiro@PREDATOREK>
2024-06-11 18:03:53 +0200

    Add project files.

---
+++ Buttons/Template.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SaladApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public partial class Template
+    {
+        public Template()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Salad_Click(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(MainPage));
+        }
+
+        private void User_Click(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(User));
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(Create));
+        }
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(Menu));
+        }
+
+        private void Cart_Click(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(Cart));
+        }
+
+        private void FindUs_Click(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(FindUs));
+        }
+
+        private void SocialMedia_Click(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(SocialMedia));
+        }
+    }
+}
---
+++ SaladAppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaladApp
+{
+    internal class SaladAppDbContext
+    {
+        public static SQLiteConnection Connection;
+        public static SQLiteCommand Command;
+
+        public static string SaladAppDB = "SaladAppDB.db";
+        public static string SaladAppDBConnection = "Data Source=" + SaladAppDB + ";Version=3;New=False;Compress=True;";
+
+        public SQLiteCommand Connect() // connection
+        {
+            try 
+            {
+                Connection = new SQLiteConnection(SaladAppDBConnection);
+                Connection.Open();
+                Command = Connection.CreateCommand();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error with connecting to database: " + ex.Message);
+                throw;
+            }
+            return Command;
+        }
+
+        public void Disconnect()
+        {
+            Connection.Close();
+        }
+    }
+}
